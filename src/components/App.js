3a9ececb362ded8e01091bb424802d99b8a486aa
pankajmokashi <pankajmokasi12@gmail.com>
2023-08-04 11:40:24 +0530

    submitting code-Fri Aug 04 2023 11:40:23 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,59 @@
+import React, { useState } from "react";
+import "./../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
-
+const API_KEY = "d14b0d23";
 const App = () => {
+  let [search, setSearch] = useState("");
+  let [data, setData] = useState("");
+  let [error, setError] = useState("");
+
+  function handleChange() {
+    fetch(`http://www.omdbapi.com/?s=${search}&apikey=${API_KEY}`)
+      .then((res) => res.json())
+      .then((res) => {
+        if(res.Response == "True"){
+          setData(res.Search);
+        }
+        else{
+          setError("Invalid movie name. Please try again.")
+        }
+      })
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <div>
+        <div>Search Movies</div>
+        <div>
+          <input
+            type="search"
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+          ></input>
+          <button onClick={handleChange}>Search</button>
+        </div>
+        <ul>
+        {
+          data.length > 0 &&
+            data.map((movie) => (
+              <li key={movie.imdbID}>
+                <div>{movie.Title}</div>
+                <div>
+                  <img src={movie.Poster} alt="img"></img>
+                </div>
+              </li>
+            ))
+        }
+        </ul>
+        {
+          error &&
+          <div className="error">{error}</div>
+        }
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
